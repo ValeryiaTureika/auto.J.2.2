28ab9378a6ee10cc38961fe46365d74d92537729
Valeria Hurava <ler4ik65@gmail.com>
2021-12-07 20:03:12 +0100

    first commit

---
+++ src/test/java/ru/netology/web/OrderingCardDelivery.java
@@ -0,0 +1,31 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.ElementsCollection;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderingCardDelivery {
+
+    String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    @Test
+    public void shouldOrderingCardDelivery() {
+        open("http://localhost:9999");
+        ElementsCollection lines = $$("[class='input__control']");
+        lines.get(0).setValue("Москва");
+        lines.get(1).doubleClick().sendKeys(Keys.BACK_SPACE);
+        lines.get(1).setValue(date);
+        lines.get(2).setValue("Иванов Иван");
+        lines.get(3).setValue("+74955105555");
+        $("[data-test-id='agreement']").click();
+        $("[class='button__content']").click();
+        $("[class='notification__content']").shouldHave(text("Встреча успешно"), Duration.ofSeconds(16));
+    }
+}
